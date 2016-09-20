545509315a5f5613206799d537b4340151ca79b3
Zach Cardais <zcardais@gmail.com>
2016-09-19 21:51:49 -0400

    Overhaul markup to use unordered list instead of fig and figcaption; overhauled styles; start of coding the overlay in jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,38 @@
+//Problem: User, when clicking on an image, goes to a dead end
+//Solution: Create an overlay with the large image - Lightbox
+
+var $overlay = $('<div id="overlay"></div>');
+//Add overlay
+$("body").append($overlay);
+  //An image
+  //A caption
+
+//Capture the click event on a link to an image.
+$("#imageGallery a").click(function(event){
+  event.preventDefault();
+  var href = $(this).attr("href");
+  $overlay.show();
+  console.log(href);
+  //  1.1 Show the overlay
+  //Update overlay with the image linked to the link
+  //Get child's alt attribute and set caption
+});
+
+//When overlay is clicked
+  //Hide the overlay
+
+
 // Hide captions in portfolio grid view
 // Show captions in lightbox view
-
 // Hide captions
-$("figure .caption").hide();
-// Add a button
-$("figure").append("<button>Lightbox</button>");
-// When button is pressed,
-$("button").click(function(){
-  // Show caption in lighbox
-  $("figure .caption").show();
-  // Get rid of button
-  $(this).remove();
-});
+// $("figure .caption").hide();
+// // Add a button
+// $("figure").append("<button>Lightbox</button>");
+// // When button is pressed,
+// $("button").click(function(){
+//   // Show caption in lighbox
+//   $(this).prev().show();
+//   // $("figure .caption").show();
+//   // Get rid of button
+//   $(this).remove();
+// });
